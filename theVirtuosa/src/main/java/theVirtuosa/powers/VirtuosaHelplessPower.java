219452c60f0572eb144876b5fede5e19dba0e02b
allieFlx <alliefleur@outlook.com>
2023-01-07 01:15:25 +0000

    updated Helpless power icon and strings

--- theVirtuosa/src/main/java/theVirtuosa/powers/VirtuosaHelplessPower.java
+++ theVirtuosa/src/main/java/theVirtuosa/powers/VirtuosaHelplessPower.java
@@ -24,8 +24,8 @@ public class VirtuosaHelplessPower extends AbstractPower implements CloneablePow
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
-    private static final Texture tex84 = TextureLoader.getTexture("theVirtuosaResources/images/powers/placeholder_power84.png");
-    private static final Texture tex32 = TextureLoader.getTexture("theVirtuosaResources/images/powers/placeholder_power32.png");
+    private static final Texture tex84 = TextureLoader.getTexture("theVirtuosaResources/images/powers/helpless_power84.png");
+    private static final Texture tex32 = TextureLoader.getTexture("theVirtuosaResources/images/powers/helpless_power32.png");
 
     // TODO Helpless keyword
 
