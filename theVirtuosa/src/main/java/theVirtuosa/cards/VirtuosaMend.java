19a91c5370b2d5687261b1445b611e792a116040
allieFlx <alliefleur@outlook.com>
2023-01-08 08:33:10 +0000

    added card: Mend [Heal 1 (2) HP. Remove 1 (2) Frail. Exhaust.]

---
+++ theVirtuosa/src/main/java/theVirtuosa/cards/VirtuosaMend.java
@@ -0,0 +1,67 @@
+package theVirtuosa.cards;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theVirtuosa.TheVirtuosa;
+import theVirtuosa.characters.TheVirtuosaCharacter;
+
+import static theVirtuosa.TheVirtuosa.makeCardPath;
+
+public class VirtuosaMend extends AbstractDynamicCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     *
+     * Heal 1 (2) HP. Remove 1 (2) Frail. Exhaust.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = TheVirtuosa.makeID(VirtuosaMend.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+
+    // /TEXT DECLARATION/
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheVirtuosaCharacter.Enums.COLOR_BROWN;
+
+    private static final int COST = 0;
+    private static final int MAGIC = 1;
+    private static final int UPGRADE_MAGIC = 1;
+
+
+    // /STAT DECLARATION/
+
+
+    public VirtuosaMend() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC;
+
+        this.exhaust = true;
+        this.tags.add(CardTags.HEALING);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new HealAction(p, p, magicNumber));
+        this.addToBot(new ReducePowerAction(p, p, "Frail", magicNumber));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC);
+            initializeDescription();
+        }
+    }
+}
