b126db7e95ed2dcc05f050e79a8564795104639a
allieFlx <alliefleur@outlook.com>
2023-01-09 17:04:24 +0000

    new card: Trill

---
+++ theVirtuosa/src/main/java/theVirtuosa/cards/VirtuosaTrill.java
@@ -0,0 +1,84 @@
+package theVirtuosa.cards;
+
+import basemod.devcommands.draw.Draw;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import theVirtuosa.TheVirtuosa;
+import theVirtuosa.characters.TheVirtuosaCharacter;
+import theVirtuosa.patches.CustomTags;
+import theVirtuosa.powers.VirtuosaResonancePower;
+
+import java.util.Iterator;
+
+import static theVirtuosa.TheVirtuosa.makeCardPath;
+
+public class VirtuosaTrill extends AbstractDynamicCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Gain 5 Resonance. Draw a card (2 cards).
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = TheVirtuosa.makeID(VirtuosaTrill.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheVirtuosaCharacter.Enums.COLOR_BROWN;
+
+    private static final int COST = 1;
+    private static final int MAGIC = 1;
+    private static final int UPGRADE_MAGIC = 1;
+    private static final int RESONANCE = 5;
+
+
+    // /STAT DECLARATION/
+
+
+    public VirtuosaTrill() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC;
+
+        this.tags.add(CustomTags.GAIN_RESONANCE);
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(
+                new ApplyPowerAction(p, p, new VirtuosaResonancePower(p, RESONANCE)));
+
+        this.addToBot(new DrawCardAction(magicNumber));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC);
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
