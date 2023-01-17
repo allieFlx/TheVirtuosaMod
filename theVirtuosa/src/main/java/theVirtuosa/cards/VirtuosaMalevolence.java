590b606e3549d17e83ea2bbe9fa5bfaede977c2f
allieFlx <alliefleur@outlook.com>
2023-01-17 14:59:54 +0000

    new card: Maleficence

---
+++ theVirtuosa/src/main/java/theVirtuosa/cards/VirtuosaMalevolence.java
@@ -0,0 +1,84 @@
+package theVirtuosa.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ModifyDamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.red.Rampage;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theVirtuosa.TheVirtuosa;
+import theVirtuosa.actions.RevealCardsAction;
+import theVirtuosa.characters.TheVirtuosaCharacter;
+
+import static theVirtuosa.TheVirtuosa.makeCardPath;
+
+public class VirtuosaMalevolence extends AbstractDynamicCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     * maleficence
+     *  Deal 9 damage. Reveal 3 cards. If none are Skills, double this card's damage this combat.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = TheVirtuosa.makeID(VirtuosaMalevolence.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheVirtuosaCharacter.Enums.COLOR_BROWN;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 9;
+    private static final int MAGIC = 3;
+    // private static final int UPGRADE_MAGIC = 1;
+
+    // /STAT DECLARATION/
+
+
+    public VirtuosaMalevolence() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // TODO doubling doesnt work
+
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SMASH));
+
+        this.addToBot(
+                new RevealCardsAction(
+                        magicNumber,
+                        c -> c.type == CardType.SKILL,
+                        matches ->
+                        {
+                            // do nothing
+                        },
+                        def -> {
+                            this.addToBot(new ModifyDamageAction(this.uuid, this.damage));
+                        }
+                )
+        );
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            initializeDescription();
+        }
+    }
+}
