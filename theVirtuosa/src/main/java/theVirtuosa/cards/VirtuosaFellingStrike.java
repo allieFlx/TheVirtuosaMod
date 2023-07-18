d55dd08e719a6b5c29ffe043abf22da79fb56df5
allieFlx <alliefleur@outlook.com>
2023-07-18 07:56:44 +0100

    added/updated Helpless keyword. new card: Felling Strike

---
+++ theVirtuosa/src/main/java/theVirtuosa/cards/VirtuosaFellingStrike.java
@@ -0,0 +1,74 @@
+package theVirtuosa.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theVirtuosa.TheVirtuosa;
+import theVirtuosa.characters.TheVirtuosaCharacter;
+import theVirtuosa.powers.VirtuosaHelplessPower;
+
+import static theVirtuosa.TheVirtuosa.makeCardPath;
+
+public class VirtuosaFellingStrike extends AbstractDynamicCard {
+
+    /*
+     Deal 7 (9) damage. Apply 1 (2) Helpless.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = TheVirtuosa.makeID(VirtuosaFellingStrike.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheVirtuosaCharacter.Enums.COLOR_BROWN;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 7;
+    private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int MAGIC = 1;
+    private static final int UPGRADE_MAGIC= 1;
+
+    // /STAT DECLARATION/
+
+
+    public VirtuosaFellingStrike() { // public ${NAME}() - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC;
+        this.tags.add(CardTags.STRIKE);
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn),
+                AbstractGameAction.AttackEffect.SLASH_HEAVY));
+        this.addToBot(new ApplyPowerAction(m, p, new VirtuosaHelplessPower(m, magicNumber), magicNumber));
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_MAGIC);
+            initializeDescription();
+        }
+    }
+}
